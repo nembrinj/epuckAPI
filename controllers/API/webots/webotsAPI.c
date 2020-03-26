7b140d14236a779b99f1a2f2c14b0ea579e96773
Julien Nembrini <julien.nembrini@unifr.ch>
2020-03-26 11:05:50 +0100

    expanded webots API for additional sensors

--- controllers/API/webots/webotsAPI.c
+++ controllers/API/webots/webotsAPI.c
@@ -71,7 +71,6 @@ void disable_led(int led_position){
 const char *prox_sensors_names[PROX_SENSORS_COUNT] =
   {"ps0", "ps1", "ps2", "ps3", "ps4", "ps5", "ps6", "ps7"};
 WbDeviceTag prox_sensor_tags[PROX_SENSORS_COUNT];
-double prox_corr_vals[PROX_SENSORS_COUNT];
 
 
 void init_prox() {
@@ -97,6 +96,46 @@ void get_prox_calibrated(short int *prox_values) {
 void calibrate_prox() {
   printf("no calibration in simulation: get_prox_calibrated() is equivalent to get_prox()\n");
 }
+
+const char *light_sensors_names[PROX_SENSORS_COUNT] =
+  {"ls0", "ls1", "ls2", "ls3", "ls4", "ls5", "ls6", "ls7"};
+WbDeviceTag light_sensor_tags[PROX_SENSORS_COUNT];
+
+
+
+
+void init_light() {
+  int i;
+  // init light sensors
+  for (i=0;  i<PROX_SENSORS_COUNT; i++) {
+    light_sensor_tags[i] = wb_robot_get_device(light_sensors_names[i]);
+    wb_light_sensor_enable(light_sensor_tags[i], TIME_STEP);
+  }
+}
+
+
+void calibrate_light() {
+  printf("no calibration in simulation: get_light_calibrated() is equivalent to get_light()\n");
+}
+
+
+
+
+void get_light(short int *light_values) {
+  int i;
+  
+  //printf("%f\n",wb_light_sensor_get_value(light_sensor_tags[3]));
+  for (i=0; i<PROX_SENSORS_COUNT; i++) {
+    light_values[i] = wb_light_sensor_get_value(light_sensor_tags[i]);
+  }
+}
+
+void get_light_calibrated(short int *light_values) {
+    get_light(light_values);
+}
+
+
+
 
 
 
@@ -132,10 +171,64 @@ WbDeviceTag cam;
 void init_camera() {
   cam = wb_robot_get_device("camera");
   wb_camera_enable(cam,CAM_RATE*TIME_STEP);
+  
+  // create dir images
+  struct stat st = {0};
+
+  if (stat("./images", &st) == -1) {
+    mkdir("./images", 0700);
+  }
+
 }
 
 void disable_camera() {
   wb_camera_disable(cam);
+}
+
+
+// https://stackoverflow.com/questions/2654480/writing-bmp-image-in-pure-c-c-without-other-libraries
+// https://en.wikipedia.org/wiki/BMP_file_format#File_structure
+
+void save_bmp_image(const unsigned char *image) {
+	static int image_counter = 0;
+	char filename[32];
+	
+	
+	sprintf(filename, "images/image%03d.bmp", image_counter++);
+
+	int width = CAMERA_WIDTH;
+	int height = CAMERA_HEIGHT;
+
+	int filesize = 54 + 3 * width * height;
+	unsigned char bmpfileheader[14] = {'B', 'M', 0, 0, 0, 0, 0, 0, 0, 0, 54, 0, 0, 0};
+	
+	bmpfileheader[2] = (unsigned char)(filesize);
+	bmpfileheader[3] = (unsigned char)(filesize >> 8);
+	bmpfileheader[4] = (unsigned char)(filesize >> 16);
+	bmpfileheader[5] = (unsigned char)(filesize >> 24);
+	
+	unsigned char bmpinfoheader[40] = {40, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 24, 0};
+	
+	bmpinfoheader[4] = (unsigned char)(width);
+	bmpinfoheader[5] = (unsigned char)(width >> 8);
+	bmpinfoheader[6] = (unsigned char)(width >> 16);
+	bmpinfoheader[7] = (unsigned char)(width >> 24);
+	bmpinfoheader[8] = (unsigned char)(height);
+	bmpinfoheader[9] = (unsigned char)(height >> 8);
+	bmpinfoheader[10] = (unsigned char)(height >> 16);
+	bmpinfoheader[11] = (unsigned char)(height >> 24);
+	
+	unsigned char bmppad[3] = {0, 0, 0};
+	
+	FILE *f = fopen(filename, "wb");
+	fwrite(bmpfileheader, 1, 14, f);
+	fwrite(bmpinfoheader, 1, 40, f);
+	for (int i = 0; i < height; i++) {
+		// https://stackoverflow.com/questions/7886196/c-pointer-1-meaning/7886232
+		fwrite(image + (width * (height - i - 1) * 3), 3, width, f);
+		fwrite(bmppad, 1, (4 - (width * 3) % 4) % 4, f);
+	}
+	fclose(f);
 }
 
 
@@ -154,7 +247,151 @@ void get_camera(unsigned char *red, unsigned char *green, unsigned char *blue) {
       blue[n*width+m] = wb_camera_image_get_blue(im, width, m, n);
     }
   } 
+  
+  unsigned char bgr888[CAMERA_WIDTH * CAMERA_HEIGHT * 3];
+
+  for (int i = 0; i < CAMERA_HEIGHT * CAMERA_WIDTH; i++) {
+		bgr888[3 * i + 2] = red[i];
+		bgr888[3 * i + 1] = green[i];
+		bgr888[3 * i] = blue[i];
+  }
+  
+  save_bmp_image(bgr888);
 }   
+
+
+
+
+
+/*
+// get inclination values
+void get_inclination(unsigned char *inclination_values) { //8-bits value
+	inclination_values[INCLINATION_FRONT] = *(unsigned char *)(&sensor[14]);
+	inclination_values[INCLINATION_RIGHT] = *(unsigned char *)(&sensor[15]);
+	inclination_values[INCLINATION_BACK] = *(unsigned char *)(&sensor[16]);
+	inclination_values[INCLINATION_LEFT] = *(unsigned char *)(&sensor[17]);
+}
+*/
+
+// //////////////
+// temperature sensor (credits: Jonas Fontana)
+
+
+// get temperature
+void get_temp(unsigned char *temp) {
+    printf("no temperature in Webots\n");
+	temp[0] = -1;
+}
+
+
+// //////////////
+// time-of-flight sensor (credits: Jonas Fontana)
+
+
+const char *tof_sensors_names[1] = {"tof"};
+WbDeviceTag tof_sensor_tags[1];
+
+
+
+
+void init_tof() {
+  // init time of flight sensor
+  tof_sensor_tags[0] = wb_robot_get_device(tof_sensors_names[0]);
+  wb_distance_sensor_enable(tof_sensor_tags[0], TIME_STEP);
+}
+
+
+// get distance given in millimeters
+void get_tof(short int *tof_distance) {
+  //printf("%f\n",wb_distance_sensor_get_value(tof_sensor_tags[0]));
+  tof_distance[0] =  wb_distance_sensor_get_value(tof_sensor_tags[0]);
+}
+
+
+// //////////////
+// accelerometer values (credits: Jonas Fontana)
+
+// get gyro
+void get_gyro_axes( short *gyro) {
+    printf("no gyroscope in Webots\n");
+	gyro[AXES_X] = -1;
+	gyro[AXES_Y] = -1;
+	gyro[AXES_Z] = -1;
+}
+
+
+
+const char *acc_sensors_names[1] =
+  {"accelerometer"};
+WbDeviceTag accelerometer_tags[1];
+
+void init_accelerometer() {
+  accelerometer_tags[0] = wb_robot_get_device(acc_sensors_names[0]);
+  wb_accelerometer_enable(accelerometer_tags[0], TIME_STEP);
+}
+
+// get orientation
+void get_orientation(float *orientation) {
+    const double* acc = wb_accelerometer_get_values(accelerometer_tags[0]);
+	orientation[0] = atan2(acc[0],acc[1]);
+}
+
+// get inclination
+void get_inclination(float *inclination) {
+    const double* acc = wb_accelerometer_get_values(accelerometer_tags[0]);
+	inclination[0] =  atan2(sqrt( acc[0]*acc[0] + acc[1]*acc[1]),acc[2]);
+}
+
+// get acceleration
+void get_acceleration(float *acceleration) {
+    const double* acc = wb_accelerometer_get_values(accelerometer_tags[0]);
+	acceleration[0] = sqrt( acc[0]*acc[0] + acc[1]*acc[1] + acc[2]*acc[2])*1000.0;
+}
+
+// get acceleration
+void get_acceleration_axes(short int *acceleration) {
+    const double* acc = wb_accelerometer_get_values(accelerometer_tags[0]);
+    //printf("%f\n",acc_val[2]);
+	acceleration[AXES_Y] = acc[1]*1000.0;
+	acceleration[AXES_X] = acc[0]*1000.0;
+	acceleration[AXES_Z] = acc[2]*1000.0;
+}
+
+
+
+
+
+
+// //////////////
+// playing sound (credits: Jonas Fontana)
+
+
+// play one of the default sounds
+void play_sound(int sound) {
+	printf("no support for playing sounds yet\n");
+}
+
+// stop sound
+void stop_sound(void) {
+	// does nothing on webots
+}
+
+void get_microphones(short int *soundlevels) {
+    printf("no microphones in Webots\n");
+	soundlevels[MICROPHONE_RIGHT] = -1;
+	soundlevels[MICROPHONE_LEFT] = -1;
+	soundlevels[MICROPHONE_BACK] = -1;
+	soundlevels[MICROPHONE_FRONT] = -1;
+}
+
+
+
+
+
+
+
+
+
 
 // ///////////////////
 // Communication
@@ -170,11 +407,6 @@ void init_communication() {
   wb_emitter_set_channel(emitter, COM_CHANNEL);
   wb_receiver_enable(receiver, TIME_STEP);
   wb_receiver_set_channel(receiver, COM_CHANNEL);
-}
-
-
-void disable_communication() {
-  // does nothing up to now....
 }
 
 // send the message
@@ -198,17 +430,6 @@ void receive_msg(char* buffer) {
 }
 
 
-int get_robot_ID() {
-    int a;
-    // parse webots name
-    const char* name = wb_robot_get_name();
-    if (sscanf(name, "%i", &a) < 1) {
-      printf("please provide an integer for the robot name in webots\n");
-      return -1;
-    }
-    else return a;
-}
-
 
 
 
@@ -224,7 +445,10 @@ void init_robot() {
 
 void init_sensors() {
   init_prox();
+  init_light();
   init_ground();
+  init_accelerometer();
+  init_tof();
 }
 
 int robot_go_on() {
--- controllers/API/webots/webotsAPI.h
+++ controllers/API/webots/webotsAPI.h
@@ -5,7 +5,10 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DEBUG 0
+// to create dir
+#include <sys/stat.h>
+#include <unistd.h>
+
 
 #include <webots/robot.h>
 #define TIME_STEP 64
@@ -13,7 +16,6 @@
 void init_robot();
 void init_sensors();
 void disable_sensors();
-int get_robot_ID();
 
 void cleanup_robot();
 
@@ -38,6 +40,7 @@ void enable_led(int led_position);
 
 /*** PROXIMITY SENSORS start ***/
 #include <webots/distance_sensor.h>
+#include <webots/light_sensor.h>
 #define PROX_SENSORS_COUNT 8
 #define NBR_CALIB 50
 #define OFFSET_CALIB 5
@@ -56,6 +59,11 @@ void enable_led(int led_position);
 void get_prox(short int *prox_values);
 void get_prox_calibrated(short int *prox_values);
 void calibrate_prox();
+
+void get_light(short int *prox_values);
+void get_light_calibrated(short int *prox_values);
+void calibrate_light();
+
 /*** PROXIMITY SENSORS end ***/
 
 
@@ -81,6 +89,87 @@ void init_camera();
 void disable_camera();
 /*** CAMERA end ***/
 
+
+
+
+
+
+
+/*** TEMPERATURE start ***/
+
+#define TEMP_SENSOR_COUNT 1
+void get_temp(unsigned char *temp);
+
+/*** TEMPERATURE stop ***/
+
+/*** TOF start ***/
+
+#define TOF_SENSOR_COUNT 1
+void get_tof(short int *tof_distance);
+
+/*** TOF stop ***/
+
+/*** ACCELEROMETER start ***/
+#include <webots/accelerometer.h>
+
+
+#define AXES_X 0
+#define AXES_Y 1
+#define AXES_Z 2
+
+// instantaneous rotational speed for each axis 
+#define GYRO_SENSOR_COUNT 3
+void get_gyro_axes( short *gyro);
+
+
+// planar orientation of acceleration vector (relative to robot)
+#define ORIENT_SENSOR_COUNT 1
+void get_orientation(float *orientation);
+
+// inclination to vertical of acceleration vector  
+#define INCLIN_SENSOR_COUNT 1
+void get_inclination(float *inclination);
+
+// magnitude of acceleration vector  
+#define ACC_SENSOR_COUNT 1
+void get_acceleration(float *acceleration);
+
+// raw acceleration values for each axis
+#define ACC_RAW_SENSOR_COUNT 3
+void get_acceleration_axes(short int *acceleration);
+
+/*** ACCELEROMETER stop ***/
+
+
+
+
+
+
+/*** SOUND start ***/
+
+void play_sound(int sound);
+void stop_sound(void);
+
+#define MICROPHONE_COUNT 4
+
+#define MICROPHONE_FRONT 0
+#define MICROPHONE_RIGHT 1
+#define MICROPHONE_BACK 2
+#define MICROPHONE_LEFT 3
+
+void get_microphones(short int *soundlevels);
+
+
+/*** SOUND stop ***/
+
+
+
+
+
+
+
+
+
 /*** COMMUNICATION start ***/
 #include <webots/emitter.h>
 #include <webots/receiver.h>
@@ -91,7 +180,6 @@ void disable_camera();
 #define MSG_LENGTH 4
 
 void init_communication();
-void disable_communication();
 void send_msg(const char *snd);
 void receive_msg(char *rcv);
 
